7c4a291432588c3deb733caa884261c1866c306f
Rebornnn <346446534@qq.com>
2018-01-02 09:20:11 +0800

    为更新做准备

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?',function(data){
+        console.log(data);
+    });
 });
+
+
+["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
